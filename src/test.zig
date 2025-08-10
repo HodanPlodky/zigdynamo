4ac508f83a8ab3b4caaaa6b6b7fdf6476771c6f5
Hodan <plodeada@fit.cvut.cz>
2025-08-10 23:15:41 +0200

    enabled utils test

--- src/test.zig
+++ src/test.zig
@@ -3,6 +3,6 @@ comptime {
     _ = @import("lexer.zig");
     _ = @import("compiler.zig");
     _ = @import("test_runtimes.zig");
-    //_ = @import("utils.zig");
+    _ = @import("utils.zig");
     _ = @import("test_snap.zig");
 }
