68df0343e7a0ddaec545dd7c2f75032057e7e776
Hodan <plodeada@fit.cvut.cz>
2026-02-09 16:23:40 +0100

    last thing in benchmark

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -4,5 +4,6 @@ MINRUNS=10
 
 hyperfine --warmup 3 --min-runs ${MINRUNS} -L kind optjit,jit,bc 'zig-out/bin/dynamo --{kind} benchmarks/whilefib.dyn' 2> /dev/null
 hyperfine --warmup 3 --min-runs ${MINRUNS} -L kind optjit,jit,bc 'zig-out/bin/dynamo --{kind} benchmarks/fib.dyn' 2> /dev/null
+hyperfine --warmup 3 --min-runs ${MINRUNS} -L kind jit,bc 'zig-out/bin/dynamo --{kind} benchmarks/linkedlist.dyn' 2> /dev/null
 
 
