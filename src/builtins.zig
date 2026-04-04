2f58770ced8099647079e12ed9c7b8be52ce288c
plodeada <plodeada@fit.cvut.cz>
2026-04-04 22:01:15 +0200

    char_to_int builtin

--- src/builtins.zig
+++ src/builtins.zig
@@ -1,9 +1,12 @@
 const runtime = @import("runtime.zig");
 const std = @import("std");
 const JitCallConv = @import("jit_utils.zig").JitCallConv;
+const ConstantIndex = @import("bytecode.zig").ConstantIndex;
+const Constant = @import("bytecode.zig").Constant;
 
 pub const Builtin = enum(u8) {
     print,
+    char_to_int,
     exit,
 };
 
@@ -23,9 +26,33 @@ pub fn BuiltinDispatch(comptime Interpreter: type) fn(*Interpreter, Builtin, u64
                     check_arg_count(arg_count, 1);
                     const value: runtime.Value = inter.stack.pop();
                     const value_type = value.get_type();
-                    std.debug.assert(value_type == runtime.ValueType.number);
+                    if (value_type != .number) {
+                        @panic("exit must get number");
+                    }
                     const number = value.get_number() % 0xff;
                     std.posix.exit(@intCast(number));
+                },
+                .char_to_int => {
+                    check_arg_count(arg_count, 1);
+                    const value: runtime.Value = inter.stack.pop();
+                    const value_type = value.get_type();
+                    if (value_type != .string) {
+                        @panic("char_to_int must get string");
+                    }
+
+                    const index = value.get_idx();
+                    const constant_idx = ConstantIndex.new(index);
+                    const constant: Constant = inter.bytecode.get_constant(constant_idx);
+                    if (constant.get_type() != .string) {
+                        @panic("char_to_int must get string");
+                    }
+                    const slice = constant.get_slice();
+                    if (slice.len != 6) {
+                        @panic("char_to_int must get string of length 1");
+                    }
+                    const number: u32 = @intCast(slice[5]);
+                    const result = runtime.Value.new_num(number);
+                    inter.stack.push(result);
                 },
             }
         }
@@ -37,6 +64,8 @@ pub fn get_builtin(name: []const u8) ?Builtin {
         return Builtin.print;
     } else if (std.mem.eql(u8, name, "exit")) {
         return Builtin.exit;
+    } else if (std.mem.eql(u8, name, "char_to_int")) {
+        return Builtin.char_to_int;
     }
 
     return null;
